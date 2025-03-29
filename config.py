3b33ca3a627f4ba0a50e646c9fd89721849ac8e9
Muhammad Reza Tribosnia <rezabosnia@gmail.com>
2025-03-29 01:21:33 +0000

    Upload carbon dataset and credentials

---
+++ config.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# config.py - Store sensitive database credentials here
+
+POSTGRES_HOST = "uclba-de25v2.cluster-cowglvndjvxv.eu-west-2.rds.amazonaws.com"
+POSTGRES_DB = "postgres"
+POSTGRES_USER = "muhammad.tribosnia.24@ucl.ac.uk"
+POSTGRES_PASSWORD = "rzMwmH"
+POSTGRES_SCHEMA = "schema_muhammadtribosnia24uclacuk"
+
+newsio_api = "pub_769248ddb51ef8ab5c1766e49e025b83cc2c7"
+gemini_api = "AIzaSyD9UDN_pSG2mFbz5oORl5R88eXXWopQ3ws"
+
+
+# In[ ]:
+
+
+
+
